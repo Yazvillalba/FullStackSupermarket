27341bb017e083a32d029f4d7a887128c5711161
mariesteberena <mesteberena@gmail.com>
2024-08-02 12:25:46 -0300

    Se agrega footer

--- milka.js
+++ milka.js
@@ -6,23 +6,22 @@ document.addEventListener('DOMContentLoaded', function() {
         { nombre: "Producto 4", precio: 25.00, img: "img/milka/producto1.webp" },
         { nombre: "Producto 5", precio: 30.00, img: "img/milka/producto2.webp" },
         { nombre: "Producto 6", precio: 35.00, img: "img/milka/producto3.webp" },
-        { nombre: "Producto 6", precio: 35.00, img: "img/milka/producto3.webp" }
-
+        { nombre: "Producto 7", precio: 30.00, img: "img/milka/producto2.webp" },
+        { nombre: "Producto 8", precio: 30.00, img: "img/milka/producto2.webp" },
     ];
 
-    const productosContainer = document.querySelector('.productos-container');
-    
-    const itemsPerSlide = 3;
-    const numberOfSlides = Math.ceil(productos.length / itemsPerSlide);
+    const productosContainer = document.querySelector('.productos-container');    
+    const itemsPorSlider = 4;
+    const cantSliders = Math.ceil(productos.length / itemsPorSlider);
 
-    for (let i = 0; i < numberOfSlides; i++) {
+    for (let i = 0; i < cantSliders; i++) {
         const carouselItem = document.createElement('div');
         carouselItem.className = `carousel-item${i === 0 ? ' active' : ''}`;
-        const productoWrapper = document.createElement('div');
-        productoWrapper.className = 'd-flex w-100';
+        const productCard = document.createElement('div');
+        productCard.className = 'd-flex w-100 ';
 
-        for (let j = 0; j < itemsPerSlide; j++) {
-            const index = i * itemsPerSlide + j;
+        for (let j = 0; j < itemsPorSlider; j++) {
+            const index = i * itemsPorSlider + j;
             if (index < productos.length) {
                 const producto = productos[index];
                 const productoElement = document.createElement('div');
@@ -34,18 +33,18 @@ document.addEventListener('DOMContentLoaded', function() {
                         <h4>${producto.nombre}</h4>
                         <p class="precio">$${producto.precio.toFixed(2)}</p>
                         <div class="producto-actions">
-                            <button class="btn btn-light like-button"><span>&#9829;</span></button>
+                            <button class="like-button"><span>&#9829;</span></button>
                             <input type="number" class="cantidad-input" value="1" min="1">
-                            <button class="btn btn-primary agregar-button">Agregar</button>
+                            <button class="btn btn-dark agregar-button">Agregar</button>
                         </div>
                     </div>
                 `;
 
-                productoWrapper.appendChild(productoElement);
+                productCard.appendChild(productoElement);
             }
         }
 
-        carouselItem.appendChild(productoWrapper);
+        carouselItem.appendChild(productCard);
         productosContainer.appendChild(carouselItem);
     }
 
